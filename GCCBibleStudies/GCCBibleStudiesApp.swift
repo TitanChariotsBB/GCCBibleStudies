c3398ab73c4aa9840040f19e6d9456c96511b6b4
Christian Abbott <cmagwoodva@gmail.com>
2024-11-18 20:06:19 -0500

    Initial Commit

---
+++ GCCBibleStudies/GCCBibleStudiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GCCBibleStudiesApp.swift
+//  GCCBibleStudies
+//
+//  Created by Christian Abbott on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GCCBibleStudiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GCCBibleStudies/Views/BibleStudyView.swift
@@ -0,0 +1,18 @@
+//
+//  BibleStudyView.swift
+//  GCCBibleStudies
+//
+//  Created by Christian Abbott on 11/18/24.
+//
+
+import SwiftUI
+
+struct BibleStudyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BibleStudyView()
+}
