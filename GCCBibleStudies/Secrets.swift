7ffc07bc4ab1b4eba807395a659bb4d4f92742ca
Nathanael Kuhns <kuhns@GCCs-Laptop.local>
2024-12-11 12:13:47 -0500

    update gitignore

---
+++ GCCBibleStudies/Secrets.swift
@@ -0,0 +1,10 @@
+//
+//  Secrets.swift
+//  GCCBibleStudies
+//
+//  Created by Nathanael Kuhns on 12/11/24.
+//
+
+import Foundation
+
+let MONGO_DB_URL: String = "mongodb+srv://TitanChariotsBB:Woahthedatabase314159@christianscluster.unx7dco.mongodb.net/GCCBibleStudies"
